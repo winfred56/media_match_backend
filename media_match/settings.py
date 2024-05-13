25c895d65585b47c2eb0ad3bce405be5e9d1e695
Winfred56 <winfred242002@gmail.com>
2024-05-13 20:09:58 +0000

    add corsheaders

--- media_match/settings.py
+++ media_match/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     "fingerprint.apps.FingerprintConfig"
-    "corsheaders"
 ]
 
 MIDDLEWARE = [
